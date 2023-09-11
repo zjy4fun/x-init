122e34df207457bc912bc5e1376b60f8bb4c3426
zjy4fun <zjy4fun@gmail.com>
2023-09-11 18:36:43 +0800

    feat: update template

--- bin/x-init.mjs
+++ bin/x-init.mjs
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 
 import { program } from 'commander'
+import { exec } from 'child_process'
 import inquirer from 'inquirer';
 import ora from 'ora';
 import download from 'download-git-repo'
 
 const templateUrls = {
-    'Vue3 + Arco Design': 'direct:https://github.com/zjy4fun/arco-vue3-template.git#main',
-    'Vue3 + Vite + Tailwind CSS': 'direct:https://github.com/zjy4fun/tailwindcss-vue3-vite-template#main'
+    'Simple': 'direct:https://github.com/zjy4fun/vue3-vite-ts-simple-template.git#main',
+    'Vue3 + Vite + Arco Design': 'direct:https://github.com/zjy4fun/arco-vue3-template.git#main',
+    'Vue3 + Vite + Tailwind CSS': 'direct:https://github.com/zjy4fun/tailwindcss-vue3-vite-template.git#main'
 }
 
 program
@@ -22,6 +24,12 @@ program
                     name: 'frameTemplate',
                     message: 'choose a frame template',
                     choices: Object.keys(templateUrls)
+                },
+                {
+                    type: 'list',
+                    name: 'git',
+                    message: 'need git init?',
+                    choices: ['yes', 'no']
                 }
             ])
             .then((answer) => {
@@ -40,10 +48,21 @@ program
 
                         } else {
                             spinner.succeed('download success')
+                            // check if git init
+                            if (answer.git === 'yes') {
+                                exec('git init', { cwd: projectName }, (err, stdout, stderr) => {
+                                    if (err) {
+                                        console.log(err)
+                                        return
+                                    }
+                                    console.log(stdout)
+                                    console.log(stderr)
+                                })
+                            }
                         }
                     }
                 )
             })
     })
 
-program.version("1.0.1").parse(process.argv)
+program.version("1.0.3").parse(process.argv)
